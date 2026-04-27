89e9b9c31328519d2078298752089e5cc779eb88
gayatri <gayatribagul46@gmail.com>
2026-04-27 15:59:20 +0530

    function

---
+++ public/home.js
@@ -0,0 +1,11 @@
+export function Heading(){
+    return "Javascript Module";
+}
+var a=10, b=10;
+ export function Addition (){
+    return a + b;
+}
+
+
+var Subtext = alert("WELCOME TO HOME PAGE")
+export default Subtext;
